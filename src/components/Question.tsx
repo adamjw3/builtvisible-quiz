9688d67cb976c9983cba85c36f06946e2491ca12
adamjw3 <367147+adamjw3@users.noreply.github.com>
2024-08-24 16:22:50 +0100

    Update Question.tsx

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -64,7 +64,7 @@ function Question() {
 
                 <button className="btn" onClick={handleNext}>
                     <span className="btn__inner">
-                        <span>{state.currentQuestionIndex === 9 ? 'Finish' : 'Next'}</span>
+                        <span>{state.currentQuestionIndex === 10 ? 'Finish' : 'Next'}</span>
                         <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.5 18L15.5 12L9.5 6" stroke="#090A0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
